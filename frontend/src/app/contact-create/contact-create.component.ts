603c1d6dee318175341451fe0fe07df742e64a76
adnier <adnier87@gmail.com>
2019-05-31 13:39:04 -0500

    form for user creation

---
+++ frontend/src/app/contact-create/contact-create.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'app-contact-create',
+  templateUrl: './contact-create.component.html',
+  styleUrls: ['./contact-create.component.css']
+})
+export class ContactCreateComponent implements OnInit {
+
+  public contactForm = new FormGroup({
+    firstName: new FormControl("", [Validators.required]),
+    lastName: new FormControl("", [Validators.required]),
+    email: new FormControl(
+      "",
+      [Validators.required,
+      Validators.pattern('[a-zA-Z0-9_\.]+@[a-zA-Z]+\.[a-zA-Z]+')]
+    ),
+    phone: new FormControl("", [Validators.pattern('[0-9]+')]),
+    city: new FormControl(),
+    country: new FormControl(),
+    title: new FormControl()
+  })
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  get email() {
+    return this.contactForm.get("email");
+  }
+
+  get phone() {
+    return this.contactForm.get("phone");
+  }
+
+  public onSubmit() {
+    console.log(this.contactForm.value);
+    this.apiService.createContact(this.contactForm.value).subscribe((res) => {
+      alert("Successfully created!");
+      this.contactForm.reset()
+    },
+    (err) => {
+      alert("Error");
+      console.log(err);
+      this.contactForm.reset();
+    })
+  }
+
+}
